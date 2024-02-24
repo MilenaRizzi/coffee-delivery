fa1a2139fe2fd81852335903430518f2c8b07b89
Milena Clara Rizzi <116590085+MilenaRizzi@users.noreply.github.com>
2024-02-24 20:11:27 -0300

    router de cart

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,15 +1,15 @@
 import {Routes, Route} from 'react-router-dom'
 import { Home } from '../pages/Home'
-import { Checkout } from '../pages/Checkout'
 import { Success } from '../pages/Success'
 import { DefaultLayout } from '../layouts/DefaultLayout'
+import { Cart } from '../pages/Cart'
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout/>}>
         <Route path="/" element={<Home/>}/>
-        <Route path="/checkout" element={<Checkout/>}/>
+        <Route path="/cart" element={<Cart/>}/>
         <Route path="/success" element={<Success/>} />
       </Route>
     </Routes>
