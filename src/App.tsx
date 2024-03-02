929da6d85c3feecad68c9a24f1381cb6fea782a7
Milena Clara Rizzi <116590085+MilenaRizzi@users.noreply.github.com>
2024-03-02 11:52:03 -0300

    aplicando o provider de context em App

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,19 @@ import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
 import { Router } from "./components/Router";
 import { BrowserRouter } from "react-router-dom";
+import { CartContextProvider } from "./contexts/CartContext";
 
 function App() {
   return (
     <>
-      <ThemeProvider theme={defaultTheme}>
-        <BrowserRouter>
-          <Router />
-        </BrowserRouter>
-        <GlobalStyle />
-      </ThemeProvider>
+      <CartContextProvider>
+        <ThemeProvider theme={defaultTheme}>
+          <BrowserRouter>
+            <Router />
+          </BrowserRouter>
+          <GlobalStyle />
+        </ThemeProvider>
+      </CartContextProvider>
     </>
   );
 }
