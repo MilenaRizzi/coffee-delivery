993046a18acba2fd15b4352fcc317bca8e0f484e
Milena Clara Rizzi <116590085+MilenaRizzi@users.noreply.github.com>
2024-03-11 17:19:51 -0300

    Update CartContext.tsx

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -1,63 +1,94 @@
-import { ReactNode, createContext, useReducer, useState } from "react";
+import { ReactNode, createContext, useEffect, useReducer } from "react";
 import { Item, Order, cartReducer } from "../reducers/cart/reducer";
-import {  addNewItemAction } from "../reducers/cart/actions";
+import {
+  addNewItemAction,
+  checkoutCartAction,
+  decrementItemQuantityAction,
+  incrementItemQuantityAction,
+  removeItemAction,
+} from "../reducers/cart/actions";
+import { NewAddressFormData } from "../pages/Cart";
+import { useNavigate } from "react-router-dom";
 // import { coffees } from "../../data.json";
 
-
 export const CartContext = createContext({} as CartContextType);
-
 
 interface CartContexProviderProps {
   children: ReactNode;
 }
 
-
 interface CartContextType {
-  cart: Item[]
-  order: Order[]
-  quantity: number;
-  quantityDecrement: () => void;
-  quantityIncrement: () => void;
+  cart: Item[];
+  orders: Order[];
   addItem: (item: Item) => void;
+  removeItem: (itemId: Item['id']) => void;
+  decrementItemQuantity: (itemId: Item["id"]) => void;
+  incrementItemQuantity: (itemId: Item["id"]) => void;
+  checkout: (order: NewAddressFormData) => void;
 }
 
 export function CartContextProvider({ children }: CartContexProviderProps) {
-  
-  const [quantity, setQuantity] = useState(1);
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const [cartState, dispatch] = useReducer(cartReducer
-    , {
-    cart: [],
-    order: []
-  })
+  const [cartState, dispatch] = useReducer(
+    cartReducer,
+    {
+      cart: [],
+      orders: [],
+    },
+    (cartState) => {
+      const storedStateAsJSON = localStorage.getItem(
+        "@coffee-delivery:cart-state-1.0.0"
+      );
 
-  const { cart, order } = cartState
+      if (storedStateAsJSON) {
+        return JSON.parse(storedStateAsJSON);
+      }
 
-  function quantityDecrement() {
-    if (quantity > 0) {
-      setQuantity(quantity - 1);
+      return cartState;
     }
-  }
+  );
 
-  function quantityIncrement() {
-    setQuantity(quantity + 1);
-  }
+  const { cart, orders } = cartState;
+  const navigate = useNavigate();
 
+  useEffect(() => {
+    if (cartState) {
+      const stateJSON = JSON.stringify(cartState);
+
+      localStorage.setItem("@coffee-delivery:cart-state-1.0.0", stateJSON);
+    }
+  }, [cartState]);
 
   function addItem(item: Item) {
-    dispatch(addNewItemAction(item))
+    dispatch(addNewItemAction(item));
   }
 
-    // setCartCoffees([...cartCoffees, item])
+  function removeItem(itemId: Item['id']) {
+    dispatch(removeItemAction(itemId))
+  }
+
+  function incrementItemQuantity(itemId: Item["id"]) {
+    dispatch(incrementItemQuantityAction(itemId));
+  }
+
+  function decrementItemQuantity(itemId: Item["id"]) {
+    dispatch(decrementItemQuantityAction(itemId));
+  }
+
+  function checkout(order: NewAddressFormData) {
+    dispatch(checkoutCartAction(order, navigate));
+  }
+
+  // setCartCoffees([...cartCoffees, item])
   return (
     <CartContext.Provider
       value={{
         cart,
-        order,
-        quantityIncrement,
-        quantityDecrement,
+        orders,
         addItem,
-        quantity,
+        removeItem,
+        incrementItemQuantity,
+        decrementItemQuantity,
+        checkout,
       }}
     >
       {children}
