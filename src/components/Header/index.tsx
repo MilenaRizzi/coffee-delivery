028a85af7974d9e683c3f18b3beb93d1cc71ab82
Milena Clara Rizzi <116590085+MilenaRizzi@users.noreply.github.com>
2024-03-11 17:18:31 -0300

    Update index.tsx

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,35 @@
-import { CustomMapPin, CustomShoppingCart, HeaderContainer, Local, ShoppingCartContainer } from "./styles";
+import { useContext } from "react";
+import { CustomMapPin, HeaderContainer, Local, Navigator} from "./styles";
+import { NavLink } from "react-router-dom";
+import { CartContext } from "../../contexts/CartContext";
+import { ShoppingCart } from "@phosphor-icons/react";
 
 export function Header() {
+
+  const { cart } = useContext(CartContext)
+  
+  let quantity: number = 0;
+
+  cart.forEach(item => {
+    quantity += item.quantity;
+  });
+
   return (
     <HeaderContainer>
       <img src="src\assets\logo.png" alt="" />
-      <nav>
+      <Navigator>
         <Local>
           <CustomMapPin size={22} weight="fill"/>
           <p>Porto Alegre, RS</p>
         </Local>
-        <ShoppingCartContainer type="button">
-          <CustomShoppingCart size={22} weight="fill" />
-        </ShoppingCartContainer>
-      </nav>
+
+        <NavLink to="/cart" title="Carrino" >
+          <ShoppingCart size={22} weight="fill" />
+          <div>
+            {cart.length > 0 ? <span>{quantity}</span> : null}
+          </div>
+        </NavLink>
+      </Navigator>
     </HeaderContainer>
   );
 }
