4f6ba680cbeb518a3841f552209403cf0b4d952a
Milena Clara Rizzi <116590085+MilenaRizzi@users.noreply.github.com>
2024-03-11 17:21:47 -0300

    use reduce

--- src/reducers/cart/actions.ts
+++ src/reducers/cart/actions.ts
@@ -1,4 +1,6 @@
+import { NavigateFunction } from "react-router-dom";
 import { Item } from "./reducer";
+import { NewAddressFormData } from "../../pages/Cart";
 
 export enum ActionTypes {
   ADD_NEW_ITEM = "ADD_NEW_ITEM",
@@ -12,7 +14,48 @@ export function addNewItemAction(item: Item) {
   return {
     type: ActionTypes.ADD_NEW_ITEM,
     payload: {
-      data: item,
-    },
+      item
+    }
   };
+}
+
+export function removeItemAction(itemId: Item['id']) {
+  return {
+    type: ActionTypes.REMOVE_ITEM,
+    payload: {
+      itemId,
+    },
+  } 
+}
+
+export function incrementItemQuantityAction(itemId: Item['id']) {
+  return {
+    type: ActionTypes.INCREMENT_ITEM_QUANTITY,
+    payload: {
+      itemId
+    }
+  }
+}
+
+export function decrementItemQuantityAction(itemId: Item['id']) {
+  return {
+    type: ActionTypes.DECREMENT_ITEM_QUANTITY,
+    payload : {
+      itemId
+    }
+  }
+}
+
+
+export function checkoutCartAction(
+  order: NewAddressFormData,
+  callback: NavigateFunction,
+) {
+  return {
+    type: ActionTypes.CHECKOUT_CART,
+    payload: {
+      order,
+      callback,
+    },
+  } 
 }
--- src/reducers/cart/reducer.ts
+++ src/reducers/cart/reducer.ts
@@ -1,37 +1,82 @@
-import { NewAddressFormData } from "../../pages/Cart"
-
+import { NewAddressFormData } from "../../pages/Cart";
+import { produce } from "immer";
+import { ActionTypes } from "./actions";
 
 export interface Item {
-  id: string
-  quantity: number
+  id: string;
+  quantity: number;
 }
 
 export interface Order extends NewAddressFormData {
-  id: number
-  items: Item[]
+  id: number;
+  items: Item[];
 }
 
 interface CartState {
-  cart: Item[]
-  order: Order[]
+  cart: Item[];
+  orders: Order[];
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function cartReducer(state: CartState, action: any) {
   switch (action.type) {
-    case'ADD_NEW_ITEM':
-    return  {
-    ...state,
-      cart: state.cart.map((item) => {
-        if(item.id === action.payload.item.id) {
-          return item.quantity += action.payload.item.quantity
+    case ActionTypes.ADD_NEW_ITEM:
+      return produce(state, (draft) => {
+        const itemAlreadyAdded = draft.cart.find(
+          (item) => item.id === action.payload.item.id
+        );
+
+        if (itemAlreadyAdded) {
+          itemAlreadyAdded.quantity += action.payload.item.quantity;
         } else {
-          return item/*cart.push(action.payload.item)*/
+          draft.cart.push(action.payload.item);
         }
-    }),
-        
-    }
+      });
+
+    case ActionTypes.REMOVE_ITEM:
+      return produce(state, (draft) => {
+        const itemToRemoveId = draft.cart.findIndex(
+          (item) => item.id === action.payload.itemId
+        );
+        draft.cart.splice(itemToRemoveId, 1);
+      });
+      
+    case ActionTypes.INCREMENT_ITEM_QUANTITY:
+      return produce(state, (draft) => {
+        const itemToIncrement = draft.cart.find(
+          (item) => item.id === action.payload.itemId
+        );
+
+        if (itemToIncrement?.id) {
+          itemToIncrement.quantity += 1;
+        }
+      });
+
+    case ActionTypes.DECREMENT_ITEM_QUANTITY:
+      return produce(state, (draft) => {
+        const itemToDecrement = draft.cart.find(
+          (item) => item.id === action.payload.itemId
+        );
+
+        if (itemToDecrement?.id && itemToDecrement.quantity > 1) {
+          itemToDecrement.quantity -= 1;
+        }
+      });
+
+    case ActionTypes.CHECKOUT_CART:
+      return produce(state, (draft) => {
+        const newOrder = {
+          id: new Date().getTime(),
+          items: state.cart,
+          ...action.payload.order,
+        };
+        draft.orders.push(newOrder);
+        draft.cart = [];
+
+        action.payload.callback(`/order/${newOrder.id}/success`);
+      });
+
+    default:
+      return state;
   }
- 
-  return state
 }
