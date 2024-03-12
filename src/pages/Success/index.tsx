a20fe86d1ceda562f680a7dc0c0e47ea6c1f6530
Milena Clara Rizzi <116590085+MilenaRizzi@users.noreply.github.com>
2024-03-12 16:01:29 -0300

    Update index.tsx

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,76 +1,88 @@
 import { CurrencyDollar, MapPin, Timer } from "@phosphor-icons/react";
-import { Info, InfoContent, Element, SuccessContainer } from "./styles";
-import { useTheme } from 'styled-components'
+import {
+  Info,
+  InfoContent,
+  Element,
+  SuccessContainer,
+  SuccessContent,
+} from "./styles";
+import { useTheme } from "styled-components";
 import { useContext } from "react";
 import { CartContext } from "../../contexts/CartContext";
 import { useParams } from "react-router-dom";
+import deliveryImage from "../../assets/delivery.svg";
+
 
 export function Success() {
-  const { orders } =  useContext(CartContext)
-  const { orderId } = useParams()
-  const orderInfo = orders.find((order) => order.id === Number(orderId))
+  const { orders } = useContext(CartContext);
+  const { orderId } = useParams();
+  const orderInfo = orders.find((order) => order.id === Number(orderId));
   const paymentMethod = {
-    credit: 'Cartão de crédito',
-    debit: 'Cartão de débito',
-    cash: 'Dinheiro',
-  }
+    credit: "Cartão de crédito",
+    debit: "Cartão de débito",
+    cash: "Dinheiro",
+  };
   const theme = useTheme();
 
   if (!orderInfo?.id) {
-    return null
+    return null;
   }
 
   return (
     <SuccessContainer>
+      <SuccessContent>
         <div>
           <h1>Uhu! Pedido confirmado</h1>
           <p>Agora é só aguardar que logo o café chegará até você</p>
-
           <Info>
             <InfoContent>
               <Element>
-                <MapPin color={theme.white}
-                style={{ backgroundColor: theme["purple-300"] }}
-                size={32} />
-               <div>
-                <span>
-                  Entrega em{' '}
-                  <strong>
-                    {orderInfo.street}, {orderInfo.residenceNumber}
-                  </strong>
-                </span>
-
-                <span>
-                  {orderInfo.neighborhood} - {orderInfo.city},{orderInfo.state}
-                </span>
-              </div>
+                <MapPin
+                  color={theme.white}
+                  style={{ backgroundColor: theme["purple-300"] }}
+                  size={32}
+                />
+                <div>
+                  <span>
+                    Entrega em{" "}
+                    <strong>
+                      {orderInfo.street}, {orderInfo.residenceNumber}
+                    </strong>
+                  </span>
+                  <span>
+                    {orderInfo.neighborhood} - {orderInfo.city},
+                    {orderInfo.state}
+                  </span>
+                </div>
               </Element>
-
               <Element>
-                <Timer color={theme.white}
-                style={{ backgroundColor: theme["yellow-300"] }}
-                size={32} />
+                <Timer
+                  color={theme.white}
+                  style={{ backgroundColor: theme["yellow-300"] }}
+                  size={32}
+                />
                 <div>
                   <span>Previsão de entrega</span>
+                  <img src="src\assets\img-coffe.png" alt="" />
                   <strong>20 min - 30 min</strong>
                 </div>
               </Element>
-
               <Element>
-                <CurrencyDollar color={theme.white}
-                style={{ backgroundColor: theme["yellow-500"] }}
-                size={32} />
-                 <div>
-                <span>Pagamento na entrega</span>
-
-                <strong>{paymentMethod[orderInfo.paymentMethod]}</strong>
-              </div>
+                <CurrencyDollar
+                  color={theme.white}
+                  style={{ backgroundColor: theme["yellow-500"] }}
+                  size={32}
+                />
+                <div>
+                  <span>Pagamento na entrega</span>
+                  <strong>{paymentMethod[orderInfo.paymentMethod]}</strong>
+                </div>
               </Element>
-
             </InfoContent>
           </Info>
         </div>
-      <img src="src\assets\delivery.svg" alt="" />
+      </SuccessContent>
+        <div id="delivery"><img src={deliveryImage} alt="" /></div>
     </SuccessContainer>
   );
 }
