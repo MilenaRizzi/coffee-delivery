167b1ac8de5edec36a92621bfb975313f44d9f76
Milena Clara Rizzi <116590085+MilenaRizzi@users.noreply.github.com>
2024-03-12 16:01:38 -0300

    Update CartContext.tsx

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -78,7 +78,6 @@ export function CartContextProvider({ children }: CartContexProviderProps) {
     dispatch(checkoutCartAction(order, navigate));
   }
 
-  // setCartCoffees([...cartCoffees, item])
   return (
     <CartContext.Provider
       value={{
